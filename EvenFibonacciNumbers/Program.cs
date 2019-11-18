f63ee29938cb09eba498ed374e7d644a101cb538
Trashed <jkortelahti@gmail.com>
2019-11-19 00:14:22 +0200

    Solved Problem 2 - 'Even Fibonacci numbers'

---
+++ EvenFibonacciNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EvenFibonacciNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int FIB_TARGET = 4000000;
+
+            List<int> sequence = new List<int>();
+
+            // Fibonacci sequence
+            int fib = 0, a = 0, b = 1;
+            do
+            {
+                fib = a + b;
+                a = b;
+                b = fib;
+
+                if (fib % 2 == 0) sequence.Add(fib);
+            } while (fib <= FIB_TARGET);
+
+            Console.WriteLine("Sum: {0}", sequence.Sum());
+        }
+    }
+}
