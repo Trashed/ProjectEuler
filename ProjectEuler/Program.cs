cde6d707aa9dc0122f1123ce21554c0c8eeb2cc1
Trashed <jkortelahti@gmail.com>
2019-11-18 23:37:46 +0200

    Solved Problem 1 - 'Multiples of 3 and 5'

---
+++ ProjectEuler/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProjectEuler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            for (int i = 0; i < 1000; i++)
+            {
+                numbers.Add(i);
+            }
+
+            // Use an anonymous function to get all multiples of three and five
+            var sum = numbers.Select(x => x).Where(x => x % 3 == 0 || x % 5 == 0).Sum();
+
+            Console.WriteLine("Sum of multiples: {0}", sum);
+        }
+    }
+}
